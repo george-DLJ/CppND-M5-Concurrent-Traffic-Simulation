c8c1d3d9486f9a7dec111d231849421181cfe749
George-DLJ <38262125+george-DLJ@users.noreply.github.com>
2021-01-04 17:44:51 +0000

    fix: last fixes on Traffic Light to improve behavior
    bugfix: the queues will be cleared before emplace_back on send and also cleared after pop_back on receive. This improves notably the behavior, but still sometimes cars keep stopped on green lights.

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -20,14 +20,16 @@ enum TrafficLightPhase
 // Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
 // Also, there should be an std::condition_variable as well as an std::mutex as private members. 
 
-//template <class T> //FP3: not used as a template!
+// FP3: not used as a template!     
+// FP7 using a template (all types renamed to T, 'lightPhase' variable to 'msg')
+template <class T> 
 class MessageQueue
 {
 public:
-    TrafficLightPhase receive();
-    void send(TrafficLightPhase &&lightPhase);
+    T receive();
+    void send(T &&msg); 
 private:
-    std::deque<TrafficLightPhase> _queue;
+    std::deque<T> _queue;
     std::condition_variable _condition;
     std::mutex _mutex;
 };
@@ -57,7 +59,7 @@ private:
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
-    MessageQueue _lightPhaseMsgs;
+    MessageQueue<TrafficLightPhase> _lightPhaseMsgs;
     TrafficLightPhase _currentPhase;
     std::condition_variable _condition;
     std::mutex _mutex;
